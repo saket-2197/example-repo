79ab1e47e2f4d202cbae16707250c5ebbee21c0e
saket <saketkamble21@gmail.com>
2023-02-14 14:54:44 +0530

    BUG -101 resolve

--- exmple.java
+++ exmple.java
@@ -6,10 +6,13 @@ public class Example {
 
 // changed the code-1
 // changes BBBBBB
+        //Working on BUG -101
+        public static void getInfor () {
+            System.out.println("this is bug 101 and i am still working on bug 101");
+        }
 
 //Resolving the BUG -102
-        publi ststic void setInfo(){
+        publi ststic void setInfo () {
             System.out.println("performing chaanges for BUG -102 ");
         }
     }
-}
