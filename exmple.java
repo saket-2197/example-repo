94d0e2e700a778d66ad3923a587e136d741d0755
Saket <saketkamble92@gmail.com>
2023-02-14 13:53:18 +0530

    changes made to exmple.java

--- exmple.java
+++ exmple.java
@@ -2,5 +2,6 @@ public class Example{
 	public static void main(String[] args) {
 		System.out.println("deom java commit");
 
+	System.out.println("This is commited by saket-2 colloberator");
 	}
 }s
